13d72028e4ca4abc79724d3c37c476b7edd3df12
guam <56258709+lisqu16@users.noreply.github.com>
2022-04-19 22:01:02 +0200

    setup bot

---
+++ bot.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/diamondburned/arikawa/v2/bot"
+	"github.com/diamondburned/arikawa/v2/bot/extras/middlewares"
+	"github.com/diamondburned/arikawa/v2/gateway"
+	"github.com/joho/godotenv"
+)
+
+type Bot struct {
+	Ctx *bot.Context
+}
+
+func (bot *Bot) Setup(sub *bot.Subcommand) {
+	sub.AddMiddleware(bot.Test, middlewares.GuildOnly(bot.Ctx))
+}
+
+func (b *Bot) Test(m *gateway.MessageCreateEvent, a string) (string, error) {
+	if a == "" {
+		return "szto", nil
+	}
+	return a, nil
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Panicln("gimme token")
+	}
+
+	cmds := &Bot{}
+	bot.Run(token, cmds, func(c *bot.Context) error {
+		c.HasPrefix = bot.NewPrefix(",")
+		c.Gateway.AddIntents(gateway.IntentGuildMessages)
+
+		return nil
+	})
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/lisqu16/andrzej
+
+go 1.18
+
+require (
+	github.com/diamondburned/arikawa/v2 v2.1.0
+	github.com/joho/godotenv v1.4.0
+)
+
+require (
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+)
